f5a443ce848bd0131ccf1584c8d0fff23ca232fb
Milos9924 <mgvozdenovic9924si@raf.rs>
2025-11-06 21:35:16 +0100

    prosečna ocena studenata na predmetu za raspon godina

--- studsluzba_server/src/main/java/org/raflab/studsluzba/repositories/PolozenPredmetRepository.java
+++ studsluzba_server/src/main/java/org/raflab/studsluzba/repositories/PolozenPredmetRepository.java
@@ -5,7 +5,20 @@ import org.raflab.studsluzba.model.PolozenPredmet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PolozenPredmetRepository extends JpaRepository<PolozenPredmet, Long> {
     Page<PolozenPredmet> findByStudentskiIndeks(Indeks studentskiIndeks, Pageable pageable);
+
+    @Query("SELECT AVG(p.ocena) " +
+            "FROM PolozenPredmet p " +
+            "WHERE p.predmet.id = :predmetId " +
+            "AND p.izlazakNaIspit IS NOT NULL " +
+            "AND FUNCTION('YEAR', p.izlazakNaIspit.datumIzlaska) BETWEEN :godinaOd AND :godinaDo")
+    Double findProsecnaOcenaZaPredmetUGodinama(
+            @Param("predmetId") Long predmetId,
+            @Param("godinaOd") int godinaOd,
+            @Param("godinaDo") int godinaDo);
+
 }
