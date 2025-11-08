d32d3e81b0407cf1a2f9ca1b8c3b830895b26b74
Milos9924 <mgvozdenovic9924si@raf.rs>
2025-11-08 18:35:09 +0100

    rezultati ispita u vidu sortiranog spiska studenata

--- studsluzba_server/src/main/java/org/raflab/studsluzba/repositories/IzlazakIspitRepository.java
+++ studsluzba_server/src/main/java/org/raflab/studsluzba/repositories/IzlazakIspitRepository.java
@@ -2,6 +2,16 @@ package org.raflab.studsluzba.repositories;
 
 import org.raflab.studsluzba.model.IzlazakIspit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IzlazakIspitRepository extends JpaRepository<IzlazakIspit, Long> {
+    @Query("SELECT iz FROM IzlazakIspit iz " +
+            "WHERE iz.prijavaIspita.ispit.id = :ispitId " +
+            "ORDER BY iz.prijavaIspita.studentskiIndeks.studijskiProgram.oznaka, " +
+            "iz.prijavaIspita.studentskiIndeks.godinaUpisa, " +
+            "iz.prijavaIspita.studentskiIndeks.brojIndeksa")
+    List<IzlazakIspit> findRezultatiByIspitSorted(@Param("ispitId") Long ispitId);
 }
