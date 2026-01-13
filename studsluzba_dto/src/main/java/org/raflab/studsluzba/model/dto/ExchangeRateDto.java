b9bfc6f79efd75004cb6e872bf936b42efb7f957
dxmv <dimitirjestepanovic1@gmail.com>
2026-01-13 20:11:25 +0100

    student dto

--- studsluzba_dto/src/main/java/org/raflab/studsluzba/model/dto/ExchangeRateDto.java
+++ studsluzba_dto/src/main/java/org/raflab/studsluzba/model/dto/ExchangeRateDto.java
@@ -1,6 +1,5 @@
 package org.raflab.studsluzba.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,25 +11,19 @@ public class ExchangeRateDto {
     private String code;
     private LocalDate date;
 
-    @JsonProperty("date_from")
     private LocalDate dateFrom;
 
     private Integer number;
     private BigDecimal parity;
 
-    @JsonProperty("cash_buy")
     private BigDecimal cashBuy;
 
-    @JsonProperty("cash_sell")
     private BigDecimal cashSell;
 
-    @JsonProperty("exchange_buy")
     private BigDecimal exchangeBuy;
 
-    @JsonProperty("exchange_middle")
     private BigDecimal exchangeMiddle;
 
-    @JsonProperty("exchange_sell")
     private BigDecimal exchangeSell;
 }
 
