5b948cf0150899da823e7e17e2ab3c97c2695987
dxmv <dimitirjestepanovic1@gmail.com>
2026-01-13 19:32:58 +0100

    pocetak klijent

---
+++ studsluzba_desktopclient/src/main/java/org/raflab/studsluzbadesktopclient/controllers/MenuBarController.java
@@ -0,0 +1,44 @@
+package org.raflab.studsluzbadesktopclient.controllers;
+
+import org.raflab.studsluzbadesktopclient.MainView;
+import org.raflab.studsluzbadesktopclient.services.StudentService;
+import org.springframework.stereotype.Component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+
+@Component
+public class MenuBarController { 
+
+	final MainView mainView;
+	final StudentService studentService;
+
+	@FXML
+	private MenuBar menuBar;
+
+    public MenuBarController(StudentService studentService, MainView mainView){
+        this.studentService = studentService;
+        this.mainView = mainView;
+    }
+
+	public void openSearchStudent() {
+		mainView.changeRoot("searchStudent");
+	}
+
+	public void openNewStudent() {
+		mainView.changeRoot("newStudent");
+	}
+
+	public void openEventsPage(){
+		mainView.changeRoot("events");
+	}
+
+	public void openReportsPage(){
+		mainView.changeRoot("reports");
+	}
+
+	@FXML
+    public void initialize() {		
+
+    }
+}
